f3eb8b4b979c66a35a49a39ffc347c853ee1f788
aaqilsihab <aaqil@searchnscore.com>
2022-04-19 21:22:22 +0530

    Path file

---
+++ Path.py
@@ -0,0 +1,8 @@
+import time
+import os
+from selenium.webdriver.chrome.service import Service
+
+ROOT_DIR = os.path.abspath(os.curdir)
+print("Path :"+ROOT_DIR)
+# ser= Service(ROOT_DIR+'\chromedriver.exe')
+# gd = (ROOT_DIR+'\chromedriver.exe')
