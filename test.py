2bf52477b57d58ceba1704074e4aa3a94fe84554
aaqilsihab <aaqil@searchnscore.com>
2022-04-20 08:13:05 +0530

    test what

--- test.py
+++ test.py
@@ -53,9 +53,9 @@ print("Logged out WA")
 
 time.sleep(1)
 log_out2 = '//div[text()="Log out"]'
-log_l = driver.find_element(By.XPATH, log_out2) # driver.find_elements_by_xpath("//div[text()='Log out']")
-text_length = log_l.is_displayed()
-if (text_length == True):
+# log_l = driver.find_element(By.XPATH, log_out2) # driver.find_elements_by_xpath("//div[text()='Log out']")
+# text_length = log_l.is_displayed()
+if (driver.find_element(By.XPATH, log_out2).is_displayed() == True):
    # m= log_l.text
    input_logof = wait.until(EC.presence_of_element_located((By.XPATH, log_out2)))
    input_logof.click()
